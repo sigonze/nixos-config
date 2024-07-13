4f49a193695341653be3f503d555f182ae2b902f
sigonze <sigonze@proton.me>
2024-07-13 12:10:09 +0200

    cleanup

--- configuration.nix
+++ configuration.nix
@@ -16,11 +16,11 @@
     boot.loader.systemd-boot.enable = true;
     boot.loader.efi.canTouchEfiVariables = true;
     
+    # Enable zram
     zramSwap.enable = true;
 
     networking.hostName = "nixos"; # Define your hostname.
     # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
     # Configure network proxy if necessary
     # networking.proxy.default = "http://user:password@proxy:port/";
     # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
@@ -55,9 +55,6 @@
         isNormalUser = true;
         description = "Nicolas";
         extraGroups = [ "networkmanager" "wheel" "games" "scanner" "lp" ];
-        packages = with pkgs; [
-        #  thunderbird
-        ];
     };
 
     # Enable the X11 windowing system.
@@ -87,17 +84,15 @@
     environment.gnome.excludePackages = (with pkgs; [
         gnome-tour
         gnome-connections
-        #gnome-console
         snapshot
         gnome.gnome-shell-extensions
     ]) ++ (with pkgs.gnome; [
-        baobab # disk usage analyzer
-        epiphany # web browser
-        #simple-scan # document scanner
-        totem # video player
-        yelp # help viewer
-        evince # document viewer
-        geary # email client
+        baobab          # disk usage analyzer
+        epiphany        # web browser
+        totem           # video player
+        yelp            # help viewer
+        #evince          # document viewer
+        geary           # email client
         gnome-calendar 
         gnome-characters
         gnome-clocks 
@@ -127,79 +122,35 @@
         alsa.enable = true;
         alsa.support32Bit = true;
         pulse.enable = true;
-        # If you want to use JACK applications, uncomment this
-        #jack.enable = true;
-
-        # use the example session manager (no others are packaged yet so this is enabled by default,
-        # no need to redefine it in your config for now)
-        #media-session.enable = true;
     };
 
-    # Enable touchpad support (enabled default in most desktopManager).
-    # services.xserver.libinput.enable = true;
 
     # Allow unfree packages
     nixpkgs.config.allowUnfree = true;
 
-    # Install Firefox.
     programs.firefox.enable = true;
-
-    # Install Steam
-    programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
-        dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
-        localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
-    };
+    programs.steam.enable = true;
 
     # List packages installed in system profile. To search, run:
     # $ nix search wget
-    environment.systemPackages = with pkgs; [
-        #htop
-        fastfetch
-       
+    environment.systemPackages = with pkgs; [     
         vim
         git
         gnumake
         gcc
+        #htop
+        fastfetch
+        #wget
         
         gnomeExtensions.dash-to-dock
         gnomeExtensions.appindicator
         gnomeExtensions.caffeine
         gnome.gnome-tweaks
-        #wget
-
+        
         bitwarden
         discord
         vscodium
     ];
   
-
-    # Some programs need SUID wrappers, can be configured further or are
-    # started in user sessions.
-    # programs.mtr.enable = true;
-    # programs.gnupg.agent = {
-    #   enable = true;
-    #   enableSSHSupport = true;
-    # };
-
-    # List services that you want to enable:
-
-    # Enable the OpenSSH daemon.
-    # services.openssh.enable = true;
-
-    # Open ports in the firewall.
-    # networking.firewall.allowedTCPPorts = [ ... ];
-    # networking.firewall.allowedUDPPorts = [ ... ];
-    # Or disable the firewall altogether.
-    # networking.firewall.enable = false;
-
-    # This value determines the NixOS release from which the default
-    # settings for stateful data, like file locations and database versions
-    # on your system were taken. It‘s perfectly fine and recommended to leave
-    # this value at the release version of the first install of this system.
-    # Before changing this value read the documentation for this option
-    # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
     system.stateVersion = "24.05"; # Did you read the comment?
-
 }
