3824ae38b97924c28039ac2dcb359a9da9cc8ce6
sigonze <sigonze@proton.me>
2024-11-10 06:21:35 +0100

    update gnome default configuration

--- desktop/dconf-gnome.nix
+++ desktop/dconf-gnome.nix
@@ -4,6 +4,7 @@
     programs.dconf.profiles.user = {
         databases = [{
             settings = {
+                # Gnome
                 "org/gnome/desktop/interface" = {
                     color-scheme = "prefer-dark";
                     enable-hot-corners = true;
@@ -13,15 +14,24 @@
                 "org/gnome/mutter" = {
                     check-alive-timeout = lib.gvariant.mkInt32 30000;
                     dynamic-workspaces = true;
+                    edge-tiling = true;
+                    experimental-features = ["variable-refresh-rate"];
                 };
                 "org/gnome/desktop/wm/preferences" = {
                     theme = "adw-gtk3-dark";
                 };
-                "org/gnome/settings-daemon/plugins/xsettings" = {
-                    theme-name = "adw-gtk3-dark";
+                "org/gnome/desktop/wm/preferences" = {
+                    button-layout = "appmenu:minimize,maximize,close";
                 };
-                "org/gnome/nautilus/icon-view" = {
-                    default-zoom-level="small-plus";
+
+                # Extensions
+                "org/gnome/shell" = {
+                    enabled-extensions = [
+                        "caffeine@patapon.info"
+                        "gsconnect@andyholmes.github.io"
+                        "appindicatorsupport@rgcjonas.gmail.com"
+                        "dash-to-dock@micxgx.gmail.com"
+                    ];
                 };
                 "org/gnome/shell/extensions/dash-to-dock" = {
                     always-center-icons = true;
@@ -44,23 +54,24 @@
                     show-trash = false;
                     transparency-mode = "FIXED";
                 };
-                "org/gnome/shell" = {
-                    enabled-extensions = lib.gvariant.mkArray [
-                        "caffeine@patapon.info"
-                        "gsconnect@andyholmes.github.io"
-                        "appindicatorsupport@rgcjonas.gmail.com"
-                        "dash-to-dock@micxgx.gmail.com"
-                    ];
-                    favorite-apps = lib.gvariant.mkArray [
-                        "org.gnome.Nautilus.desktop"
-                        "org.gnome.Console.desktop"
-                        "codium.desktop"
-                        "org.mozilla.firefox.desktop"
-                        "com.bitwarden.desktop.desktop"
-                        "com.discordapp.Discord.desktop"
-                        "com.valvesoftware.Steam.desktop"
-                        "com.heroicgameslauncher.hgl.desktop"
-                    ];
+
+
+                # Applications
+                "org/gnome/nautilus/icon-view" = {
+                    default-zoom-level="small-plus";
+                };
+                "org/gnome/TextEditor" = {
+                    indent-style = "space";
+                    restore-session = false;
+                    show-line-numbers = true;
+                    show-map = true;
+                    spellcheck = false;
+                    tab-width = lib.gvariant.mkUint32 4;
+                    wrap-text = false;
+                };
+                "org/gnome/simple-scan" = {
+                    paper-height=lib.gvariant.mkUint32 2970;
+                    paper-width=lib.gvariant.mkUint32 2100;
                 };
             };
         }];
