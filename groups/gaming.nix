3fc9c1d7d8e455177882e59fc29822c751c2ca55
sigonze <sigonze@proton.me>
2024-11-18 09:36:50 +0100

    change mangohud time format

--- groups/gaming.nix
+++ groups/gaming.nix
@@ -48,7 +48,7 @@ in
         STEAM_EXTRA_COMPAT_TOOLS_PATHS="\${HOME}/.steam/root/compatibilitytools.d";
         MANGOHUD=1;
         # MANGOHUD_CONFIG = "horizontal,table_columns=16,cpu_stats,gpu_stats,ram,vram,fps,frametime=0,frame_timing=1,hud_no_margin,cpu_power,gpu_power,time";
-        MANGOHUD_CONFIG="horizontal,table_columns=16,cpu_stats,gpu_stats,ram,vram,fps,frametime=0,frame_timing=0,hud_no_margin,time,time_format=%X";
+        MANGOHUD_CONFIG="horizontal,table_columns=16,cpu_stats,gpu_stats,ram,vram,fps,frametime=0,frame_timing=0,hud_no_margin,time,time_format=%H\\:%M";
     };
 
     # add all users to group gamemode
