36d25fc67f8f9728e2bca511328dfe0e7cdecb66
sigonze <sigonze@proton.me>
2025-09-27 15:59:03 +0200

    update macbook pro config

--- src/hosts/macbook/hardware-configuration.nix
+++ src/hosts/macbook/hardware-configuration.nix
@@ -8,32 +8,35 @@
     [ (modulesPath + "/installer/scan/not-detected.nix")
     ];
 
-  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usb_storage" "usbhid" "sd_mod" ];
+  boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "uas" "usb_storage" "sd_mod" ];
   boot.initrd.kernelModules = [ ];
-  boot.kernelModules = [ "kvm-intel" "wl" ];
-  boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+  boot.kernelModules = [ "kvm-intel" ];
+  boot.extraModulePackages = [ ];
 
   fileSystems."/" =
-    { device = "/dev/disk/by-uuid/63d3c7d8-f6ba-4871-ba54-71a3ec6d01da";
+    { device = "/dev/disk/by-uuid/2287e82b-603e-4340-981b-f1c800d51b50";
       fsType = "ext4";
     };
 
   fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/C97A-29F7";
+    { device = "/dev/disk/by-uuid/827B-8128";
       fsType = "vfat";
       options = [ "fmask=0077" "dmask=0077" ];
     };
 
-  swapDevices =
-    [ { device = "/dev/disk/by-uuid/12482572-9bfa-4759-86ea-7607b8e3c998"; }
-    ];
+  fileSystems."/home" =
+    { device = "/dev/disk/by-uuid/e3b52eb5-04f1-4101-a805-5e1f9fcf96c2";
+      fsType = "ext4";
+    };
+
+  swapDevices = [ ];
 
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
   # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
   networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlp0s20u1.useDHCP = lib.mkDefault true;
+  # networking.interfaces.wlp2s0.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
--- src/hosts/macbook/host-configuration.nix
+++ src/hosts/macbook/host-configuration.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
     imports = [
@@ -6,7 +6,7 @@
     ];
 
     # Hostname
-    networking.hostName = "nix-mac";
+    networking.hostName = "macbook";
 
     # Users
     users.users.nicolas = {
@@ -15,20 +15,55 @@
         extraGroups = [ "networkmanager" "wheel" ];
     };
 
-    users.users.ruthanna = {
-        isNormalUser = true;
-        description = "Ruthanna";
-        extraGroups = [ "networkmanager" ];
-    };
+#    users.users.ruthanna = {
+#        isNormalUser = true;
+#        description = "Ruthanna";
+#        extraGroups = [ "networkmanager" ];
+#    };
 
     # hardware.facetimehd.enable = true;
 
     # Configure keyboard variant
     services.xserver.xkb.variant = "mac";
 
+    services.acpid.enable = true;
+    powerManagement.enable = true;
+    # powerManagement.cpuFreqGovernor = "schedutil";
+    services.mbpfan.enable = true;
+    hardware.cpu.intel.updateMicrocode = true;
+
+    # Add Wifi
+    nixpkgs.config.allowInsecurePredicate = pkg: builtins.elem (lib.getName pkg) [
+        "broadcom-sta"
+    ];
+    boot.initrd.kernelModules = [ "wl" ];
+    boot.kernelModules = [ "wl" "applesmc" "i915" ];
+    # boot.kernelParams = [ "hid_apple.iso_layout=0" "acpi_backlight=vendor" "acpi_mask_gpe=0x15" ];
+    boot.extraModulePackages = [ config.boot.kernelPackages.broadcom_sta ];
+
+    # Add Intel Haswell codecs
+    nixpkgs.config.packageOverrides = pkgs: {
+        intel-vaapi-driver = pkgs.intel-vaapi-driver.override { enableHybridCodec = true; };
+    };
+
+    hardware.graphics = {
+        enable = true;
+        extraPackages = with pkgs; [
+            vaapiIntel
+            vaapiVdpau
+            libvdpau-va-gl
+            intel-media-driver
+        ];
+    };
+
+    # Fix GTK Haswell support
+    environment.variables = {
+        GSK_RENDERER = "ngl";
+    };
+
     # Select Apps
     apps.devtools = true;
 
     # Initial installation version (should not be modified)
-    system.stateVersion = "24.05";
+    system.stateVersion = "25.05";
 }
